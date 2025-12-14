0dd6d6dd18234383bc3420d8788af8aec377dc89
Benjamin Arkorful <benprimeartist@gmail.com>
2025-12-14 11:27:43 +0000

    Updated js file with quotes array and function to display

---
+++ dom-manipulation/script.js
@@ -0,0 +1,32 @@
+const quotes = [
+  {
+    text: "Simplicity is the ultimate sophistication.",
+    category: "design"
+  },
+  {
+    text: "Code is poetry.",
+    category: "programming"
+  },
+  {
+    text: "Discipline beats motivation.",
+    category: "life"
+  }
+];
+
+const quoteDisplay = document.getElementById('quoteDisplay');
+const quoteButton = document.getElementById('newQuote');
+
+
+function showRandomQuote() {
+  
+  const index = Math.floor(Math.random() * quotes.length);
+  quoteDisplay.textContent = quotes[index].text
+
+}
+
+quoteButton.addEventListener('click', showRandomQuote)
+
+
+function AddQuoteForm() {
+  
+}
